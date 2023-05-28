e5f706a496ee422f5c6304897fa25786a14ba4bf
Ismaeel <miwleaf@gmail.com>
2023-05-28 14:17:19 +0500

    Add Homepage and Navbar

--- calorlyte/src/App.js
+++ calorlyte/src/App.js
@@ -1,8 +1,13 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './webpages/Home'
 
 function App() {
   return (
-    <h1>Test</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ calorlyte/src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Navbar() {
+  return (
+    <h1>Hi</h1>
+  )
+}
