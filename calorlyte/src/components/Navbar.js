09b7bbbd489d79beb9b34277b4df4968283d6cf5
Ismaeel <miwleaf@gmail.com>
2023-05-30 19:25:07 +0500

    Style Navbar and Background

--- calorlyte/src/components/Navbar.js
+++ calorlyte/src/components/Navbar.js
@@ -3,13 +3,19 @@ import styled from 'styled-components'
 
 export default function Navbar() {
   
-    const Nav = styled.div`
-
-    `
-  
     return (
-    <Nav>
+      
 
-    </Nav>
+<nav class="border-gray-200 dark:bg-gray-900">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="#" class="flex items-center">
+        
+        <span class="self-center text-4xl font-semibold whitespace-nowrap text-mod-teal">Calorlyte</span>
+    </a>
+    <button type="button" class="text-mod-white font-bold bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl hover:text-blue-900 hover:transition ease-in-out delay-250 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-xl px-20 py-5 text-center mr-2 mb-2">Get Started</button>
+  </div>
+</nav>
+
+
   )
 }
---
+++ calorlyte/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'mod-teal': '#00d6e0',
+        'mod-black': '#222831',
+        'mod-white': '#EEEEEE',
+      },
+    },
+  },
+  plugins: [],
+}
